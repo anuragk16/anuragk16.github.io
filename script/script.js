dcc627a2802e3ad060af4469ea8a0d033fca930e
Anurag kumawat <90235816+anuragk16@users.noreply.github.com>
2023-06-22 22:05:10 +0530

    Update script.js

--- script/script.js
+++ script/script.js
@@ -4,8 +4,10 @@ window.onscroll = function() {
     var top = window.scrollY;
     if(top >=100) {
         header.classList.add('navbarDark');
+        
     }
     else {
         header.classList.remove('navbarDark');
+
     }
 }
